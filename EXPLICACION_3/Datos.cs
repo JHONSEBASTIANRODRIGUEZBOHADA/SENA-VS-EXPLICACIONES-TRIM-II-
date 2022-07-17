971fb6a01f9ba68a4edeb2e1b1fbe348a46898f2
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-07-16 19:10:08 -0500

    Add files via upload

---
+++ EXPLICACION_3/Datos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXPLICACION_3
+{
+    class Datos
+    {
+        public string Name = "Watermelon";     //CREAR LAS VARIABLES DE CAMPO
+        public double Price = 3.500;
+    }
+}
---
+++ EXPLICACION_3/Mainprogram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXPLICACION_3
+{
+    class Mainprogram
+    {
+        public static void Main(string[]args)
+        {
+            //CREAR E INSTANCIAR OBJETO TIPO DATOS
+            Datos myFruit = new Datos();     //();USANDO CONSTRUCTOR VACIO //CREAR UNA INSTANCIA
+            myFruit.Name = "Pineapple";     //SE INSTANCIA
+            myFruit.Price = 5.500;
+            Console.WriteLine(myFruit.Name, myFruit.Price);     //Console= CLASE //WriteLine= METODO
+        }
+    }
+}
