e84a79a15c48a80c9c472148c1f1935790d61115
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-07-16 19:11:10 -0500

    Add files via upload

---
+++ EXPLICACION_5/Datos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+CONSTRUCTOR
+-Metodo especial
+-Usado para iniciaizar objetos
+*/
+
+namespace EXPLICACION_5
+{
+    class Datos
+    {
+        public string fruitName;     //CREAR LAS VARIABLES DE CAMPO
+        public double fruitPrice;
+
+        public Datos() { }     //CONSTRUCTOR VACIO
+        public Datos(string name,double price)     //CONSTRUCTOR USADO PARA INICIALIZAR VALORES DE LOS CAMPOS
+        {
+            this.fruitName = name;     //fruitName= HACE REFERENCIA A LA VARIABLE DE CAMPO //name= HACE REFERENCIA AL ARGUMENTO
+            this.fruitPrice=price;     //this= HACE IGUAL A LA VARIABLE DE CAMPO Y EL ARGUMENTO
+        }
+
+        public override string ToString()
+        {
+            //return "Fruta" + fruitName + "Price" + fruitPrice;
+            return $"Fruit: {fruitName} \nPrice: {fruitPrice}";     //INTERPOLACION:$""
+        }
+    }
+}
