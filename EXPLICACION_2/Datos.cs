98dcc95963c98c4854b5d9768b52285931187e1a
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-07-16 19:09:42 -0500

    Add files via upload

---
+++ EXPLICACION_2/Datos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+PILARES DE LA OOP
+-Herencia
+-Encapsulacion
+-Polimorfismo
+-Abstraccion
+-----------------------------------------------------------------------------------------------------------------------------
+ESTRUCTURA DE C#
+-spacename= para organizar los archivos
+-class= la clase con su nombre
+-metodos
+-----------------------------------------------------------------------------------------------------------------------------
+CLASE
+-Representacion / Plano para un objeto
+
+OBJETO
+-Instancia para una clase / Nombre del objeto
+
+EJEMPLO
+
+car Mycar = new car();     //como se dice= se crea e instancia un objeto de tipo car //== hace referencia a los atributos 
+
+-car= clase //de que tipo es (car)
+-Mycar= objeto //nombre de la clase
+-new= crea una instancia /para poder usar los objetos de una clase 
+-car Mycar = new car();= esto es un objeto de tipo car
+-----------------------------------------------------------------------------------------------------------------------------
+METODOS
+-Lo que es capas de hacer
+-Ejecuta comportamientos
+-Todo metodo tiene que tener parentecis
+
+-Cuando se define las variables en un metodo se le dice parametros
+-Cuando se llama las variables a un metodo se le dice argumentos
+-----------------------------------------------------------------------------------------------------------------------------
+*/
+
+namespace EXPLICACION_2
+{
+    class Datos
+    {
+        public string Nombre = "Jhon";     //CREAR LAS VARIABLES DE CAMPO
+        public string Apellido = "Rodriguez";
+    }
+}
---
+++ EXPLICACION_2/Mainprogram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXPLICACION_2
+{
+    class Mainprogram
+    {
+        public static void Main(string[]args)
+        {
+            //CREAR E INSTANCIAR OBJRTO DE TIPO DATOS
+            Datos myData = new Datos();     //();USANDO CONSTRUCTOR VACIO //CREAR UNA INSTANCIA
+            myData.Nombre = "Jhon Sebastian";
+            myData.Apellido = "Rodriguez Bohada";     //SE INSTANCIA
+            Console.WriteLine(myData.Nombre,myData.Apellido);     //Console= CLASE //WriteLine= METODO
+        }
+    }
+}
