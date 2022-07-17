96afce09fc6999214e8c5ad380522be219a836d2
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-07-16 19:10:45 -0500

    Add files via upload

---
+++ EXPLICACION_4/Datos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+CONSTRUCTOR
+-Metodo especial
+-Usado para iniciaizar objetos
+*/
+
+namespace EXPLICACION_4
+{
+    class Datos
+    {
+        public string fruitName;     //CREAR LAS VARIABLES DE CAMPO
+        public double fruitPrice;
+        public Datos()     //CONSTRUCTOR
+        { }
+
+        public Datos(string name,double price)     //CONSTRUCTOR USADO PARA INICIAIZAR VALORES DE LOS CAMPOS
+        {
+            fruitName = name;     //fruitName= HACE REFERENCIA A LA VARIABE DE CAMPO //name= HACE REFERENCIA AL ARGUMENTO
+            fruitPrice = price;
+        }
+
+        public override string ToString()
+        {
+            // return "Fruit" + fruitName + "Price" + fruitPrice;     //FORMA 1
+            return $"Fruit: {fruitName} \nPrice: {fruitPrice}";     //FORMA 2 //INTERPOLACION = $"" 
+        }
+    }
+}
---
+++ EXPLICACION_4/Mainprogram.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+PILARES DE LA OOP
+-Herencia
+-Encapsulacion
+-Polimorfismo
+-Abstraccion
+-----------------------------------------------------------------------------------------------------------------------------
+ESTRUCTURA DE C#
+-spacename= para organizar los archivos
+-class= la clase con su nombre
+-metodos
+-----------------------------------------------------------------------------------------------------------------------------
+CLASE
+-Representacion / Plano para un objeto
+
+OBJETO
+-Instancia para una clase / Nombre del objeto
+
+EJEMPLO
+
+car Mycar = new car();     //como se dice= se crea e instancia un objeto de tipo car //== hace referencia a los atributos 
+
+-car= clase //de que tipo es (car)
+-Mycar= objeto //nombre de la clase
+-new= crea una instancia /para poder usar los objetos de una clase 
+-car Mycar = new car();= esto es un objeto de tipo car
+-----------------------------------------------------------------------------------------------------------------------------
+METODOS
+-Lo que es capas de hacer
+-Ejecuta comportamientos
+-Todo metodo tiene que tener parentecis
+
+-Cuando se define las variables en un metodo se le dice parametros
+-Cuando se llama las variables a un metodo se le dice argumentos
+-----------------------------------------------------------------------------------------------------------------------------
+*/
+
+namespace EXPLICACION_4     //SE USA PARA ORGANIZAR ARCHIVOS
+{
+    class Mainprogram     //NOMBRE DE LA CLASE DE TIPO CLASS
+    {
+        /*
+        public static void Main(string[]args)       //void= NO RETORNA NINGUN VALOR      
+        {
+            //CREAR E INSTANCIAR OBJETO TIPO DATOS
+            Datos myData = new Datos("Pineapple",123.25);     //();USANDO CONSTRUCTOR VACIO //CREAR UNA INSTANCIA
+            Console.WriteLine(myData.fruitName, myData.fruitPrice);     //Console= CLASE //WriteLine= METODO
+        }
+        */
+
+        /*RECIBIR DATOS POR CONSOLA*/
+        public static void Main(string[]args)     //void= NO RETORNA NINGUN VALOR
+        {
+            Console.WriteLine("Nombre de la fruta");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Precio de la fruta");
+            double price = double.Parse(Console.ReadLine());
+
+            //CREAR E INSTANCIAR OBJETO TIPO DATOS
+            Datos myData = new Datos(name, price);      //CREO OBJETO Y PASO PARAMETROS A INICIALIZAR
+            Console.WriteLine(myData.ToString());
+        }
+    }
+}
