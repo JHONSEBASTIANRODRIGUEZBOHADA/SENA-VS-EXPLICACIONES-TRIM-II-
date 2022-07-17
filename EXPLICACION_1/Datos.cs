ea63eeb7921c9975363f85e7f158d7ca4739c33c
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-07-16 19:09:21 -0500

    Add files via upload

---
+++ EXPLICACION_1/Datos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXPLICACION_1
+{
+    class Datos
+    {
+        public string Color = "Blue";       //CREAR LAS VARIABES DE CAMPO
+        public int Maxspeed = 100;
+    }
+}
---
+++ EXPLICACION_1/Mainprogram.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+PILARES DE LA OOP
+-Herencia
+-Encapsulacion
+-Polimorfismo
+-Abstraccion
+-----------------------------------------------------------------------------------------------------------------------------
+ESTRUCTURA DE C#
+-spacename= para organizar los archivos
+-class= la clase con su nombre
+-metodos
+-----------------------------------------------------------------------------------------------------------------------------
+CLASE
+-Representacion / Plano para un objeto
+
+OBJETO
+-Instancia para una clase / Nombre del objeto
+
+EJEMPLO
+
+car Mycar = new car();     //como se dice= se crea e instancia un objeto de tipo car //== hace referencia a los atributos 
+
+-car= clase //de que tipo es (car)
+-Mycar= objeto //nombre de la clase
+-new= crea una instancia /para poder usar los objetos de una clase 
+-car Mycar = new car();= esto es un objeto de tipo car
+-----------------------------------------------------------------------------------------------------------------------------
+METODOS
+-Lo que es capas de hacer
+-Ejecuta comportamientos
+-Todo metodo tiene que tener parentecis
+
+-Cuando se define las variables en un metodo se le dice parametros
+-Cuando se llama las variables a un metodo se le dice argumentos
+-----------------------------------------------------------------------------------------------------------------------------
+*/
+
+namespace EXPLICACION_1     //SE USA PARA ORGANIZAR ARCHIVOS
+{
+    class Mainprogram     //NOMBRE DE LA CLASE DE TIPO CLASS
+    {
+        public  static void Main(string[]args)     //void= NO RETORNA NINGUN VALOR
+        {
+            //CREAR E INSTANCIAR OBJETO TIPO DATOS
+            Datos myData = new Datos();     //();USANDO CONSTRUCTOR VACIO   //CREAR UNA INSTANCIA   //SE CREA E INSTANCIA UN OBJETO LLAMADO myData DE TIPO DATOS
+            myData.Color = "green";     //SE INSTANCIA //myData= OBJETO DE CLASE //.= LLAMA A LOS ATRIBUTOS O METODOS //== INICIALIZA LOS ATRIBUTOS
+            Console.WriteLine(myData.Color);     //Console= CLASE //WriteLine= METODO
+        }
+    }
+}
